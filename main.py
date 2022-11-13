7818ed7390cb7a7e106985556c0c8047a8dcf667
Dombearx <domin.dober@gmail.com>
2022-11-13 22:44:08 +0100

    Add main

---
+++ main.py
@@ -0,0 +1,15 @@
+from game import Board
+
+
+def main():
+    board = Board()
+    board.score()
+    print(board)
+    board.mutate()
+
+    population_size = 0
+    population = [Board() for _ in range(population_size)]
+
+
+if __name__ == '__main__':
+    main()
