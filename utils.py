d5283a3c9b78f9e3f74f91bd11c428d07a7a000d
Dombearx <domin.dober@gmail.com>
2022-11-13 19:58:37 +0100

    Add cards types

---
+++ utils.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class CardType(Enum):
+    RABBIT = "Rabbit"
+    WOLF = "Wolf"
+    MEADOW = "meadow"
+    DEER = "Deer"
+    BEE = "Bee"
+    RIVER = "River"
+    EAGLE = "Eagle"
+    BEAR = "Bear"
+    FISH = "Fish"
+    DRAGONFLY = "Dragonfly"
+    FOX = "Fox"
