3c948962ce1278ba364bc180abd8a011f909bde0
Dombearx <domin.dober@gmail.com>
2022-11-18 20:09:18 +0100

    Add meadow tests

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -7,13 +7,11 @@ from utils import board_creator
 class TestRabbit:
     data = [
         (board_creator([[Rabbit]]), (0, 0), 1),
-
-        (board_creator([[Rabbit, Rabbit],
-                        [Rabbit, Rabbit]]), (0, 0), 1),
+        (board_creator([[Rabbit, Rabbit], [Rabbit, Rabbit]]), (0, 0), 1),
     ]
 
     @pytest.mark.parametrize("board, pos, score", data)
-    def test_rabbit_class(self, board, pos, score):
+    def test_rabbit_score(self, board, pos, score):
         main_card = board[pos]
 
         assert main_card is not None
@@ -23,20 +21,33 @@ class TestRabbit:
 class TestWolf:
     data = [
         (board_creator([[Wolf]]), (0, 0), 4),
-
         (board_creator([[Wolf, Wolf]]), (0, 0), 8),
-
         (board_creator([[Wolf, Rabbit]]), (0, 0), 4),
-
-        (board_creator([[Wolf, Rabbit],
-                        [Wolf, Wolf]]), (0, 0), 12),
-
-        (board_creator([[Wolf, Wolf],
-                        [Wolf, Wolf]]), (0, 0), 12),
+        (board_creator([[Wolf, Rabbit], [Wolf, Wolf]]), (0, 0), 12),
+        (board_creator([[Wolf, Wolf], [Wolf, Wolf]]), (0, 0), 12),
     ]
 
     @pytest.mark.parametrize("board, pos, score", data)
-    def test_wolf_class(self, board, pos, score):
+    def test_wolf_score(self, board, pos, score):
+        main_card = board[pos]
+
+        assert main_card is not None
+        assert main_card.score(board, {}) == score
+
+
+class TestMeadow:
+    data = [
+        (board_creator([[Meadow]]), (0, 0), 0),
+        (board_creator([[Meadow, Wolf]]), (0, 0), 0),
+        (board_creator([[Meadow, Meadow]]), (0, 0), 3),
+        (board_creator([[Meadow, Meadow], [Meadow, Wolf]]), (0, 0), 6),
+        (board_creator([[Meadow, Meadow], [Meadow, Meadow]]), (0, 0), 10),
+        (board_creator([[Meadow, Meadow], [Meadow, Wolf], [Meadow, Meadow]]), (0, 0), 15),
+        (board_creator([[Meadow, Meadow], [Meadow, Meadow], [Meadow, Meadow]]), (0, 0), 15),
+    ]
+
+    @pytest.mark.parametrize("board, pos, score", data)
+    def test_meadow_score(self, board, pos, score):
         main_card = board[pos]
 
         assert main_card is not None
